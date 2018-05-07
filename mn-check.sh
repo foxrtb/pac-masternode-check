0fd97b4b0c196a7f659adb55c4ad81e8975dc946
FoxD <foxrtb@gmail.com>
2018-05-07 12:39:40 +0300

    Update mn-check.sh

--- mn-check.sh
+++ mn-check.sh
@@ -5,7 +5,7 @@
 #set -x
 
 MN="Masternode-1"
-MNVIN="<VIN TX>"
+MNVIN="<vim tx number>"
 sentime=`stat -c %Y $HOME/sentinel/sentinel-cron.log`
 nowtime=`date -d "now - 2 hours"  +%s`
 
@@ -16,15 +16,15 @@ nowtime=`date -d "now - 2 hours"  +%s`
 ##
 if [  `ps ax | grep -v grep | grep paccoind | wc -l` -ne 1 ]
 then
-  echo paccoind-not-running on $MN
+         /usr/local/bin/pb push "paccoind not running on $MN"
 fi
 
 ##
 ## Check if MASTERNODE is in state enabled
 ##
-if [  `$HOME/paccoin-cli  masternode list |grep $MNVIN|grep -vw ENABLED|wc -l` -gt 0 ] 
+if [  `$HOME/paccoin-cli  masternode list |grep $MNVIN|grep -vw ENABLED|wc -l` -gt 0 ]
 then
-        echo Masternode-not-status-enabled $MN
+        /usr/local/bin/pb push "MN not in status ENABLED on $MN" 
 
 fi
 
@@ -33,5 +33,5 @@ fi
 ## Check if sentinel log changed recently
 ##
 if [ "$sentime" -gt "$nowtime" ]; then
-    echo sentinel-log-changed on $MN
+    /usr/local/bin/pb push "Sentinel-cron log changed  on $MN"             
 fi
